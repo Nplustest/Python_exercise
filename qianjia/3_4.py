fbd941d477ec9d503d352c1cb3f395a0b49b0c27
HawaiiQian <489678957@qq.com>
2018-09-18 11:01:43 +0800

    p72

---
+++ qianjia/3_4.py
@@ -0,0 +1,31 @@
+#def
+'''
+def hel(name):
+    print('Hello '+name)
+hel('Hawaii')
+'''
+
+import random
+def getAnswer(num):
+    if num==1:
+        return 'one'
+    elif num==2:
+        return 'two'
+'''
+r=random.randint(0,3)
+print(r)
+fortune=getAnswer(r)
+print(fortune)
+'''
+#print(getAnswer(random.randint(0,3)))
+
+#print(print('Hello'))
+
+#关键字参数
+'''
+print('Hello ',end='')
+print('Hawaii')
+
+print('a','b','c','d')
+print('a','b','c','d',sep='$$')
+'''
