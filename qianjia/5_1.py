0a8c5f073087c4971bee3f05fc82ef4f71988dcd
HawaiiQian <489678957@qq.com>
2018-09-25 17:37:36 +0800

    P105

---
+++ qianjia/5_1.py
@@ -0,0 +1,14 @@
+#字典
+myCat={'size':'fat','weight':100,'color':'white and black','age':5,10:'ten'}
+print(myCat['color'])
+print(myCat[10])
+
+print('---------------------------')
+
+a=[1,2,3]
+b=[2,3,1]
+print(a==b)
+
+app={1:'a',2:'b',3:'c'}
+bnn={2:'b',1:'a',3:'c'}
+print(app==bnn)
