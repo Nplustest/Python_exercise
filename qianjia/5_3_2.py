932154b978af14fab6c0c1e3e1176a9a04c4fc7b
HawaiiQian <489678957@qq.com>
2018-09-29 13:26:51 +0800

    P115

--- qianjia/5_3_2.py
+++ qianjia/5_3_2.py
@@ -45,12 +45,19 @@ def totalBrought(name):
     for k,v in name.items():
         for i,j in v.items():
             thing=thing+str(j)+'个'+i+','
-            '''
-             if j==5:
-                print('LALAL')
-            else:
-                print('没有')
-            '''
-
         print(k+'带来了'+thing[:-1])
 totalBrought(all)
+
+print('----------------------3')
+
+#合并带来的东西
+def totalFood(guests,food):
+    sum = 0
+    for k,v in guests.items():
+        sum=sum+v.get(food,0)
+    return sum
+
+a='苹果有'+str(totalFood(all,'apple'))+'个'
+b='香蕉有'+str(totalFood(all,'banner'))+'个'
+print(a)
+print(b)
